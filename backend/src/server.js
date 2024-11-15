ace74f4151874ac87760be03f6ace6a5ab5da129
Ranjit jana <jranjit367@gmail.com>
2024-11-16 01:28:22 +0530

    setting up for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 const { app } = require(".");
 const { connectDb } = require("./config/db");
-
-const PORT=5454;
+const port=process.env.PORT|| 4000;
 app.listen(PORT,async ()=>{
     await connectDb()
     console.log("ecommerce api listing on port ",PORT)
