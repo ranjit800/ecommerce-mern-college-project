25814bb720358bad2cec16eae8469f61805a19c4
Ranjit jana <jranjit367@gmail.com>
2024-11-16 01:35:47 +0530

    change port

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const { app } = require(".");
 const { connectDb } = require("./config/db");
-const port=process.env.PORT|| 4000;
+const port=process.env.PORT;
 app.listen(PORT,async ()=>{
     await connectDb()
     console.log("ecommerce api listing on port ",PORT)
